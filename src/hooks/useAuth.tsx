5f9b8a339f6b0cdae846c0a524c22e88fadd1729
Aaron Nguyen <nghoangviet2000@hotmail.com>
2023-03-02 10:55:54 +0700

    Update useAuth.tsx

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -3,6 +3,7 @@ import { getUserProfile } from '../services/api'
 import { useQuery } from '@tanstack/react-query'
 export const useAuth = () => {
     const { data } = useQuery({ queryKey: ['get-profile'], queryFn: getUserProfile })
+    console.log(data)
     return (
         {
             user: data?.data
